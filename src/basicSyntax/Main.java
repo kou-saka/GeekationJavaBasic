3cca410cc015c269d781ac0f00f841e711897808
kou-saka <juntakasaka5240@gmail.com>
2025-04-18 11:07:52 +0900

    クラス分けで実装

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -4,7 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello Geekation");
+		int age = 25;
+		double height = 190.5;
+		
+		int num1 = 10;
+		int num2 = 30;
+		
+		
+		//これでgreetingメソッドを呼ぶ
+		GeekIntroduction.greeting(age,height);	
+		
+		GeekIntroduction.specialSkill(num1, num2);
 	}
+	
 
 }
